21f1aea77dc53241bcec13076c6fbcaa4cd4cefd
soezimaster <soezimaster@gmail.com>
2012-03-07 22:14:08 +0900

    for update

--- draw/qdr_group.h
+++
@@ -1,7 +0,0 @@
-#ifndef __QDR_GROUP__
-#define __QDR_GROUP__
-
-#include <time.h>
-#include "qdr_private.h"
-
-#endif
--- draw/qdr_version.h
+++
@@ -1,7 +0,0 @@
-#ifndef __QDR_VERSION__
-#define __QDR_VERSION__
-
-#include "qdr_private.h"
-#include "pikl/pikl.h"
-
-#endif
